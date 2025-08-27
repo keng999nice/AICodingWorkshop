1163bd90fd3614b50411c716d01a3d52b564ede4
Aekapong Wanitchakornkoon <>
2025-08-27 13:20:16 +0700

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        pastel: {
-          pink: '#FFD3E1',
-          lavender: '#E6E6FA',
-          mint: '#B8F2B8',
-          peach: '#FFCBA4',
-          sky: '#B8E6FF',
-          yellow: '#FFF4B8',
-          purple: '#DDA0DD',
-          coral: '#FFB3BA',
-          sage: '#C8E6C9',
-          cream: '#FFF8DC'
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
