aa479af768eac56a3fa8c9b3ef56b0815908b07e
Aekapong Wanitchakornkoon <>
2025-08-27 11:47:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <p>Welcome to React with Vite</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
